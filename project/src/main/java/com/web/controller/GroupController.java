d8afff3d3bfe602c32f51fd36a45ab65b5939b69
k <k@DESKTOP-ALGGUT1>
2023-12-12 12:30:38 +0900

    마잎 페이지 완성

--- project/src/main/java/com/web/controller/GroupController.java
+++ project/src/main/java/com/web/controller/GroupController.java
@@ -1,29 +1,33 @@
 package com.web.controller;
 
-import java.security.Principal;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.Session;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.web.service.GroupService;
 import com.web.vo.GroupVO;
 import com.web.vo.MemberVO;
+import com.web.vo.PartyMember;
+import com.web.vo.PaytableVO;
 
-@SessionAttributes("member")
+import oracle.net.aso.m;
+
+
+@SessionAttributes({"member","paytable"})
 @Controller
 public class GroupController {
 	
@@ -71,16 +75,90 @@ public class GroupController {
 		groupService.groupUpdate(groupVo);
 		return "forward:index";
 	}
+	@GetMapping("KingModify")
+	public String KingModify() {
+		return "/creatparty/KingModify";
+	}
 	//=======================================================================
 	//마이페이지내정보보기
 	@GetMapping("getmypage")
-	public String getmypage(Model model,
+	public String getmypage(Model model,HttpSession session,
 							@ModelAttribute("member") MemberVO memberVo) {
+		
+//		//참여 joingroup
+//		PartyMember partymember = (PartyMember)session.getAttribute("accout");
+//		if(partymember !=null) {
+//		List<PartyMember> account = groupService.Account(partymember.getSeq());
+//		model.addAttribute("joinlist",account);	
+//		}
+		//방장 Creategroup
+		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+		List<GroupVO> mykinglist = groupService.MyKingList(memberVO.getId());
+		model.addAttribute("mykinglist",mykinglist);
+		//전체 Allgroup
 		List<GroupVO> groupList = groupService.getGroupList();
 		model.addAttribute("groupList",groupList);
-		
-		
 		return "/createparty/getmypage";
 	}
+	//방장리스트
+	@GetMapping("MyKingList")
+	public String MyKingList(Model model, HttpSession session) {
+		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+		
+		List<GroupVO> mykinglist = groupService.MyKingList(memberVO.getId());
+		model.addAttribute("mykinglist",mykinglist);
+		return "/createparty/MyKingList";
+	}
+//	System.out.println(memberVO.toString());
+//	System.out.println(memberVO.getId());
+//	return "0";
+	
+	
+	
+	
 	//=======================================================================
+	//테스트리스트
+	//리스트 이미지
+	
+	@GetMapping("listtest")
+	public String listtest(Model model,HttpSession session,
+							@Param("pseq")Integer pseq,@Param("userid") String userid){
+		//전체 Allgroup
+		List<GroupVO> groupList = groupService.getGroupList();
+		model.addAttribute("groupList",groupList);
+//		
+//		//참여파티 불러오기
+		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+		int seq = memberVO.getSeq();
+		String id = memberVO.getId();
+		model.addAttribute("joinlist", groupService.JoinList(seq,id));
+		
+		
+		return"/createparty/listtest";
+	}
+
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project/src/main/java/com/web/service/GroupService.java
@@ -0,0 +1,34 @@
+package com.web.service;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+
+import com.web.vo.GroupVO;
+import com.web.vo.MemberVO;
+import com.web.vo.PartyMember;
+import com.web.vo.PaytableVO;
+
+public interface GroupService {
+	
+	void groupInsert(GroupVO groupVo);
+	
+	//파티수정
+	void groupUpdate(GroupVO groupVo);
+	
+	//파티리스트
+	List<GroupVO> getGroupList();
+	//방장리스트
+	List<GroupVO> MyKingList(String userId);
+	//참여리스트
+//	List<PartyMember> Account(int account);
+//==============================================================
+	
+//	//리스트테스트
+	List<GroupVO> JoinList(int seq,String userid);
+//	
+
+	
+
+	
+}
